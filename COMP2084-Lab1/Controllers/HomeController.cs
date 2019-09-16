ac0daefd46bad5bff7fcf1e1316630e0239dc846
Nvi Mai <nvi.mai@expogain.com>
2019-09-16 19:04:11 -0400

    Add project files.

---
+++ COMP2084-Lab1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMP2084_Lab1.Models;
+
+namespace COMP2084_Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Addition(int x, int y)
+        {
+            return Ok("Result = " + (x + y).ToString());
+        }
+
+        public IActionResult Multiplication(int x, int y)
+        {
+            return Ok("Result = " + (x * y).ToString());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
